ee41d4ae92d26b67e09bf42048dc71bc1061a699
EoDy <61904030+DannyLin-0713@users.noreply.github.com>
2021-06-27 15:34:20 +0400

    Update passport_score_show.py

--- passport_score_show.py
+++ passport_score_show.py
@@ -24,19 +24,5 @@ def indexreq():
             print('\nwrong country code, please try again\n')
             break
 
-def country_code_search():
-    while True:
-        with open('country_hen.csv',newline='') as csvfile:
-            country_name=str(input("please input the country name to search the country code \n"))
-            rows = csv.DictReader(csvfile)
-            try:
-                abb_show = [row[country_name] for row in rows]
-                abb_refine = str(abb_show[0])
-                print('\nthe country code of ' + country_name + " is "  +abb_refine)
-                break
-            except KeyError:
-                print('\nCountry is not in the database\n')
-
 while True:
-#    country_code_search()
     indexreq()
